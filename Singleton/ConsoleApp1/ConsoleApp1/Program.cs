ba739ebf3283543b804be81507b7657be7720f5b
msawicki <msawicki@it-dev.pl>
2018-06-02 17:47:27 +0200

    Singleton

---
+++ Singleton/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton");
+
+            World obiekt1 = World.Utworz();
+            obiekt1.GetInstance();
+            World.Utworz();
+            obiekt1.GetInstance();
+            World.Utworz();
+            Console.WriteLine("Teraz juz nie moge utworzyc instancji klasy World.");
+            World abc = World.Utworz();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/ConsoleApp1/ConsoleApp1/World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class World
+    {
+        private static World instancja=null;
+        
+        // konstruktor musi byc provate aby uniemozliwic utworzenie obiektu za pomaca
+        // operatora new
+        private World()
+        {
+        }
+
+        public World GetInstance()
+        {
+            return instancja;
+        }
+        public static World Utworz()
+        {
+            if (instancja == null)
+            {
+                Console.WriteLine("Obiekt nie istnieje.");
+                instancja = new World();
+                return instancja;
+                
+            }
+            Console.WriteLine("Obiekt juz istnieje!");
+            return instancja;
+        }
+    }
+}
