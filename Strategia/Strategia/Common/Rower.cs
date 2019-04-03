0ef2403bc49dacd0e7f7ac15e5746f0fb732ceee
marcelsawicki <marcel.sawicki@gmail.com>
2019-04-03 22:36:30 +0200

    Strategia

---
+++ Strategia/Strategia/Common/Rower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategia
+{
+	class Rower : IDojezdzac
+	{
+		public void Dojezdzaj()
+		{
+			Console.WriteLine("Dojazd: rower.");
+		}
+	}
+}
---
+++ Strategia/Strategia/Pracownik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategia
+{
+	class Pracownik
+	{
+		private IPracowac pracowac = null;
+		private IDojezdzac dojezdzac = null;
+		private ISpedzanieWolnegoCzasu spedzanieWolnegoCzasu = null;
+
+		public Pracownik(string zawod)
+		{
+			if ((zawod.ToUpper()).Equals("MECHANIK"))
+			{
+			}
+			else if ((zawod.ToUpper()).Equals("LEKARZ"))
+			{
+			}
+			else if ((zawod.ToUpper()).Equals("LISTONOSZ"))
+			{
+				pracowac = new RoznoszenieListow();
+				dojezdzac = new Rower();
+		
+			}
+		}
+
+	}
+}
