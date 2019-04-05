857893671ac20dc54441aa7ffd03a97ebf0eeebc
Marcel Sawicki <marcel.sawicki@gmail.com>
2019-04-05 20:28:30 +0200

    Fabryka Komputerow - Init

---
+++ Fabryka/FabrykaKomputerow/FabrykaKomputerow/FabrykaKomputerow/Model/PC/DyskDlaPC.cs
@@ -0,0 +1,13 @@
+﻿using FabrykaKomputerow.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabrykaKomputerow.Model
+{
+    class DyskDlaPC : IDysk
+    {
+    }
+}
