66dccc4fd1b797fe7d3b3522ab4cc6c2d66b3f13
Marcel Sawicki <msawicki@it-dev.pl>
2019-10-22 13:59:28 +0200

    Demo

--- Fabryka/FabrykaTS/main.ts
+++ Fabryka/FabrykaTS/main.ts
@@ -10,5 +10,4 @@ p2.printDetails();
 let p3 = factory.getPerson(new Date(1969, 0, 20));
 p3.printDetails()
 
-let anchor = document.getElementById('anchor');
-anchor.innerText = p1.printDetailsHTML() + p2.printDetailsHTML() + p3.printDetailsHTML();
+console.log('Done')
