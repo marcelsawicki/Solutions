450c76ada503942d95266154f9890eb2ac2c3302
Marcel Sawicki <msawicki@it-dev.pl>
2019-11-03 18:07:59 +0100

    Refactor

---
+++ Demo/Factory/ComputerFactory2012/FactoryApp1/FactoryApp1/Common/Abstract/Computer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryApp1.Common.Abstract
+{
+    abstract class Computer
+    {
+        protected IDrive dysk;
+        protected IGraph grafika;
+        protected IRam ram;
+        protected IProcessor procesor;
+        protected ICooler chlodzenie;
+
+        public abstract void Assembly();
+
+        public void InstallSoftware()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Installing software...");
+        }
+
+        public void Packing()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Equipment packing...");
+        }
+
+        public void Sell()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Equipment selling...");
+        }
+    }
+}
---
+++ Demo/Factory/ComputerFactory2012/FactoryApp1/FactoryApp1/Common/IComponentsFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryApp1.Common
+{
+    interface IComponentsFactory
+    {
+        IDrive ProduceDrive();
+        IGraph ProduceGraph();
+        IRam ProduceRam();
+        IProcessor ProduceProcessor();
+        ICooler ProduceCooler();
+    }
+}
