d72d338e85f570bdad07e3cfd660c847dfe3d608
Marcel Sawicki <msawicki@it-dev.pl>
2019-11-06 21:24:42 +0100

    Startegy Translate

---
+++ Demo/Strategy/CSharp/Startegy2012/ConsoleApplication1/ConsoleApplication1/Pracownik.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Employee
+	{
+		private IWork work = null;
+		private IDrive drive = null;
+		private ISpendFreeTime spendFreeTime = null;
+
+		public Employee(string occupation)
+		{
+			if ((occupation.ToUpper()).Equals("MECHANIC"))
+			{
+                work = new NaprawaSamochodow();
+                drive = new Samochod();
+                spendFreeTime = new Silownia();
+			}
+			else if ((occupation.ToUpper()).Equals("DOCTOR"))
+			{
+                work = new Leczenie();
+                drive = new Samochod();
+                spendFreeTime = new LiteraturaPopularnoNaukowa();
+			}
+			else if ((occupation.ToUpper()).Equals("POSTMAN"))
+			{
+				work = new RoznoszenieListow();
+				drive = new Rower();
+                spendFreeTime = new Silownia();
+			}
+		}
+
+        public void Methods()
+        {
+            work.Pracuj();
+            drive.Dojezdzaj();
+            spendFreeTime.SpedzajWolnyCzas();
+        }
+    }
+}
---
+++ Demo/Strategy/CSharp/Startegy2012/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            do
+            {
+                Console.Write("Occupation (doctor, mechanic, postman): ");
+                string occupation = Console.ReadLine();
+                try
+                {
+                    Employee employee = new Employee(occupation);
+                    employee.Methods();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong profession.");
+                }
+                Console.WriteLine("End - finishes program, <ENTER> - choose profession");
+                command = Console.ReadLine();
+            }
+            while (!(command.ToUpper()).Equals("END"));
+        }
+    }
+}
