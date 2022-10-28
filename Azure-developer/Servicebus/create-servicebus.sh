ac154e59cb3d551629b8519890809f6ad72b2096
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-10-28 15:21:04 +0200

    create svcbus

---
+++ Azure-developer/Servicebus/create-servicebus.sh
@@ -0,0 +1,8 @@
+myLocation=westeurope
+myNameSpaceName=az204svcbus$RANDOM
+az group create --name az204-svcbus-rg --location $myLocation
+az servicebus namespace create --resource-group az204-svcbus-rg --name $myNameSpaceName --location $myLocation
+
+az servicebus queue create --resource-group az204-svcbus-rg --namespace-name $myNameSpaceName --name az204-queue
+
+az group delete --name az204-svcbus-rg --no-wait
