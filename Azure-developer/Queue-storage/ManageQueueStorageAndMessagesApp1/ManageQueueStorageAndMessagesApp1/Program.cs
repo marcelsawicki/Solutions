6d045e4b5ae3ed6cb0c589c498545387b630b5c6
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-11-04 12:54:34 +0100

    Update

--- Azure-developer/Queue-storage/ManageQueueStorageAndMessagesApp1/ManageQueueStorageAndMessagesApp1/Program.cs
+++ Azure-developer/Queue-storage/ManageQueueStorageAndMessagesApp1/ManageQueueStorageAndMessagesApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 using Microsoft.Extensions.Configuration;
 
 public class Program 
@@ -23,9 +24,46 @@ public class Program
 
         if (queueClient.Exists())
         {
+            QueueProperties properties = queueClient.GetProperties();
+
+            // Retrieve the cached approximate message count.
+            int cachedMessagesCount = properties.ApproximateMessagesCount;
+
+            // Display number of messages.
+            Console.WriteLine($"Number of messages in queue: {cachedMessagesCount}");
+
+            Console.WriteLine("Podaj tresc nowej message?");
+            string bodyMessage = Console.ReadLine();
             // Send a message to the queue
-            string message = string.Format("Hello world!");
+            string message = string.Format(bodyMessage);
             var result = queueClient.SendMessage(message);
+
+            // Get the message from the queue
+            QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+            Console.WriteLine("Czy nadpisac message? (tak/nie)");
+            string updateMessage = Console.ReadLine();
+            if (updateMessage == "tak")
+            {
+                // Update the message contents
+                queueClient.UpdateMessage(retrievedMessage[0].MessageId,
+                        retrievedMessage[0].PopReceipt,
+                        "Updated contents",
+                        TimeSpan.FromSeconds(60.0)  // Make it invisible for another 60 seconds
+                    );
+            }
+
+                // Process (i.e. print) the message in less than 30 seconds
+                Console.WriteLine($"Dequeued message: '{retrievedMessage[0].Body}'");
+
+            Console.WriteLine("Czy skasować pierwsza message z kolejki? (tak/nie)");
+            string deleteMessage = Console.ReadLine();
+            if (deleteMessage == "tak")
+            {
+                // Delete the message
+                queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+            }
+            
         }
     }
 }
