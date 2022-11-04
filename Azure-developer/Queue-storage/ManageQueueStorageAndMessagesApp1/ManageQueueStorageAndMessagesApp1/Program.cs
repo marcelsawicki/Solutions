8a69082e529c9af217819a81377d1695866fad0c
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-11-04 12:30:26 +0100

    Init

---
+++ Azure-developer/Queue-storage/ManageQueueStorageAndMessagesApp1/ManageQueueStorageAndMessagesApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+
+public class Program 
+{
+
+    public static void Main(string[] args)
+    {
+        // Build a config object, using env vars and JSON providers.
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        
+        var connectionString = config.GetSection("ConnectionStringBlobStorage").Value.ToString();
+        var queueName = config["QueueNameBlobStorage"];
+
+        QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+        // Create the queue if it doesn't already exist
+        queueClient.CreateIfNotExists();
+
+        if (queueClient.Exists())
+        {
+            // Send a message to the queue
+            string message = string.Format("Hello world!");
+            var result = queueClient.SendMessage(message);
+        }
+    }
+}
