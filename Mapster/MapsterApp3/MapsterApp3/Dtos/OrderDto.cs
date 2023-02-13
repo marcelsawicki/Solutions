1fac6aeda8ff662e4239750e1a8a3baaf36a0ede
Marcel Sawicki <marcel.sawicki@gmail.com>
2023-02-13 16:50:19 +0100

    Map

---
+++ Mapster/MapsterApp3/MapsterApp3/Dtos/OrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MapsterApp3.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<OrderLineDto> Lines { get; set; }
+    }
+}
