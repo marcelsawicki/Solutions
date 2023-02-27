cc8bae02b5c7f48d478adb3c306fb2e01920d472
Marcel Sawicki <marcel.sawicki@gmail.com>
2023-02-27 21:10:24 +0100

    items list

---
+++ Angular/1-items-list/items-list/src/app/todoItem.ts
@@ -0,0 +1,11 @@
+export class TodoItem {
+    
+    constructor(taskVal: string, completeVal: boolean = false) {
+        this.task = taskVal;
+        this.complete = completeVal;
+
+    }
+
+    task: string;
+    complete: boolean;
+}
